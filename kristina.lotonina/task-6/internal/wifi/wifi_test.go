d748fbdcb93ece8c54d4a7f9d8091fdf5bf0742d
kristina <kristinalotonina@yandex.ru>
2025-12-21 22:54:57 +0300

    [TASK-6] go generate

--- kristina.lotonina/task-6/internal/wifi/wifi_test.go
+++ kristina.lotonina/task-6/internal/wifi/wifi_test.go
@@ -1,3 +1,5 @@
+//go:generate mockery --name=WiFiHandle --testonly --quiet --outpkg wifi_test --output .
+
 package wifi_test
 
 import (
@@ -7,20 +9,17 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	mockwifi "github.com/kef1rch1k/task-6/internal/wifi/mocks"
 	mwifi "github.com/mdlayher/wifi"
 
 	"github.com/kef1rch1k/task-6/internal/wifi"
 )
-
-//go:generate mockery --name=WiFiHandle --testonly --quiet --outpkg wifi_test --output .
 
 var errIntf = errors.New("interfaces error")
 
 func TestGetAddresses(t *testing.T) {
 	t.Parallel()
 
-	mockWiFi := mockwifi.NewWiFiHandle(t)
+	mockWiFi := NewWiFiHandle(t)
 
 	mockWiFi.On("Interfaces").Return([]*mwifi.Interface{
 		{
@@ -39,7 +38,7 @@ func TestGetAddresses(t *testing.T) {
 func TestGetAddresses_Error(t *testing.T) {
 	t.Parallel()
 
-	mockWiFi := mockwifi.NewWiFiHandle(t)
+	mockWiFi := NewWiFiHandle(t)
 
 	mockWiFi.On("Interfaces").
 		Return(nil, errIntf)
@@ -53,7 +52,7 @@ func TestGetAddresses_Error(t *testing.T) {
 func TestGetNames(t *testing.T) {
 	t.Parallel()
 
-	mockWiFi := mockwifi.NewWiFiHandle(t)
+	mockWiFi := NewWiFiHandle(t)
 
 	mockWiFi.On("Interfaces").Return([]*mwifi.Interface{
 		{Name: "wlan0"},
@@ -70,7 +69,7 @@ func TestGetNames(t *testing.T) {
 func TestGetNames_Error(t *testing.T) {
 	t.Parallel()
 
-	mockWiFi := mockwifi.NewWiFiHandle(t)
+	mockWiFi := NewWiFiHandle(t)
 
 	mockWiFi.On("Interfaces").
 		Return(nil, errIntf)
